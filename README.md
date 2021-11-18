96e419c12cab8e85e8f8a683b65cf05333f6e073
James Pace <jpace121@gmail.com>
2021-11-18 02:56:54 +0000

    Cleanup.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-sudo apt install mosquitto-dev g++ cmake libmosquitto-dev mosquitto-clients
 
+# Dependencies
+sudo apt install mosquitto-dev g++ cmake libmosquitto-dev mosquitto-clients
 sudo apt install openssl libssl-dev
 
-
+# Generating Ed25519 Keys for Testing
 openssl genpkey -algorithm Ed25519 -out priv.key
 openssl pkey -in priv.key -pubout > pub.key
--- src/jwp-plugin.cpp
+++ src/jwp-plugin.cpp
@@ -1,28 +1,24 @@
-extern "C" {
-    #include "mosquitto.h"
-    #include "mosquitto_broker.h"
-    #include "mosquitto_plugin.h"
-}
+// Copyright 2021 James Pace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <jwp-plugin/jwp-plugin.h>
+#include <jwp-plugin/Authorizer.hpp>
 #include <string>
 #include <memory>
-#include <jwp-plugin/Authorizer.hpp>
-
-// Stuff we're "exporting" for the dynamic loading.
-extern "C" {
-    int mosquitto_plugin_version(int supported_version_count, const int *supported_versions);
-    int mosquitto_plugin_init(mosquitto_plugin_id_t *identifier, void **userdata, struct mosquitto_opt *options, int option_count);
-    int mosquitto_plugin_cleanup(void *userdata, struct mosquitto_opt *options, int option_count);
-}
-// My functions
-int jwp_auth_basic_auth_callback(int event, void *event_data, void *userdata);
-int jwp_acl_check_callback(int event, void *event_data, void *userdata);
-int jwp_disconnect_callback(int event, void *event_data, void *userdata);
-
 
 // Mosquitto Globals
 static mosquitto_plugin_id_t *plugin_id = nullptr;
 static std::unique_ptr<Authorizer> authorizer = nullptr;
-
 
 int mosquitto_plugin_version(int supported_version_count, const int *supported_versions)
 {
