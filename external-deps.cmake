4f267f5edfc82fdf2d319a01df5670f4c5ea10fa
James Pace <jpace121@gmail.com>
2022-03-26 20:10:14 -0400

    Add packaging.

--- external-deps.cmake
+++ external-deps.cmake
@@ -1,12 +1,5 @@
 include(FetchContent)
 
-FetchContent_Declare(googletest
-    GIT_REPOSITORY "https://github.com/google/googletest.git"
-    GIT_TAG "release-1.11.0"
-    GIT_SHALLOW "True"
-)
-FetchContent_MakeAvailable(googletest)
-
 FetchContent_Declare(jwt-cpp
   GIT_REPOSITORY "https://github.com/Thalhammer/jwt-cpp.git"
   GIT_TAG "v0.5.2"
